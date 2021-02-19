16fba00b0c892f41049921a38a2dcac29bd1ac36
gcpetri <gcpetri@outlook.com>
2021-02-19 09:58:59 -0600

    build folder

---
+++ build/my_gui_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gcpetri/catkin_ws/src/my_gui_pkg
+# Build directory: /home/gcpetri/catkin_ws/build/my_gui_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
