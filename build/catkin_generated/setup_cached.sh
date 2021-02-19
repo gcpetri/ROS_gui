8772f37d58a2fa14a3d36930fc9c267bd2993e14
gcpetri <gcpetri@outlook.com>
2021-02-19 11:13:50 -0600

    b

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,3 +8,4 @@
 
 # modified environment variables
 export PWD='/home/gcpetri/catkin_ws/build'
+export PYTHONPATH="/home/gcpetri/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
--- build/my_gui_pkg/cmake_install.cmake
+++ build/my_gui_pkg/cmake_install.cmake
@@ -33,6 +33,10 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 endif()
 
 if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  include("/home/gcpetri/catkin_ws/build/my_gui_pkg/catkin_generated/safe_execute_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/gcpetri/catkin_ws/build/my_gui_pkg/catkin_generated/installspace/my_gui_pkg.pc")
 endif()
 
@@ -45,5 +49,17 @@ endif()
 
 if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_gui_pkg" TYPE FILE FILES "/home/gcpetri/catkin_ws/src/my_gui_pkg/package.xml")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/my_gui_pkg" TYPE PROGRAM FILES "/home/gcpetri/catkin_ws/src/my_gui_pkg/scripts/my_gui_pkg")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_gui_pkg" TYPE DIRECTORY FILES "/home/gcpetri/catkin_ws/src/my_gui_pkg/resource")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_gui_pkg" TYPE FILE FILES "/home/gcpetri/catkin_ws/src/my_gui_pkg/plugin.xml")
 endif()
 
